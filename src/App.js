0e94b243831e2379972cac146c147e23bcc428ff
zubair shaikh <shaikhzuber@rapidinnovation.dev>
2022-09-29 11:41:28 +0530

    Fisrt Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import News from './Component/News';
+
+function App() {
+  return (
+    <div>
+    <News/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/News.js
@@ -0,0 +1,48 @@
+import React, {useState,  useEffect} from 'react';
+import axios from 'axios';
+import './News.css';
+
+
+const News = () => {
+  const [news, setNews] =  useState([]);
+
+  ///////////// fetch Rest Newsapi using react axios
+
+  useEffect(() => {
+    const loadNews = async () => {
+const responce = await axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=59443656f0e74d46b55070e02da79298");
+setNews(responce.data.articles);
+      
+    };
+    loadNews()
+  }, []);
+
+  //console.log('news', news);
+
+  return (
+    /////// renders the api elements in jsx
+
+    <div className="News">
+       <h1> Reputeted News India</h1>
+      {
+        news && news.map((item,index) => {
+          return(
+           
+            <div key={index} className='news-box'>
+              {/*<span>{item.author}</span>
+              <span>{item.content}</span>
+          */}
+              <h2 className='headline'>{item.title}</h2>
+              <p className='description'>{item.description}</p>
+              <img alt='image' src={item.urlToImage}/>
+              <a className='direct-btn' href={item.url}>Read More</a>
+            </div>
+          )
+        })
+      }
+  
+    </div>
+  );
+}
+
+export default News;
